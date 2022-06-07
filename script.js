75863c0274e093189519f8647b029517e249967f
Sam Bardsley <93604147+SamBardsley@users.noreply.github.com>
2022-06-07 15:42:25 +0000

    some js

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ btn.addEventListener('click', function handleClick() {
     btn.querySelector('ion-icon').setAttribute('name', 'sunny-outline');
   }
 });
+
+var myInputs = document.querySelectorAll(".grid");
+var wordleWord = "smart";
+
+console.log(myInputs);
+
+for (var i=0; i<myInputs.length; i++) {
+  myInputs[i].addEventListener("change", checkLetter);
+}
+
+function checkLetter(e) {
+  alert("this changed");
+  console.log(e);
+  console.log(this);
+  alert(this.value);
+}
